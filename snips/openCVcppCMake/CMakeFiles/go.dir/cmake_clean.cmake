518d6e74f53ce821957e758fa4ab8bfd371a4128
Charles Fox <charles.fox@gmail.com>
2018-04-02 17:51:03 +0100

    add snisp

---
+++ snips/openCVcppCMake/CMakeFiles/go.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/go.dir/videoWrite.cpp.o"
+  "go.pdb"
+  "go"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/go.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
